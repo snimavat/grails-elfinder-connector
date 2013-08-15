ae94b12765a5fa386b8fa305a348fd5b2390a52c
Sudhir Nimavat <sudhir_nimavat@yahoo.com>
2013-08-15 15:54:08 +0530

    Updated readme

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ grails-elfinder-connector
 =========================
 
 Elfinder file browser connector for grails
+
+### How to use.
+*Step 1:*
+Configure root directory using `grails.plugin.elfinder.rootDir = "PATH_TO_ROOT_DIR"`
+
+*Step 2:*
+Now point your elfinder client to index action of *ElfinderConnectorController*
+
+Example:
+```
+		<script> 
+			$().ready(function() {
+				var elf = $('#elfinder').elfinder({
+					url : '${g.createLink(controller:'elfinderConnector')}'				
+				}).elfinder('instance');
+			});			
+		</script>
+
+```
+
+
--- src/groovy/grails/plugin/elfinder/command/ElfinderOpenCommand.groovy
+++ src/groovy/grails/plugin/elfinder/command/ElfinderOpenCommand.groovy
@@ -6,32 +6,32 @@ package grails.plugin.elfinder.command
  *
  */
 class ElfinderOpenCommand extends ElfinderBaseCommand {
-	
-	public void execute() {		
+
+	public void execute() {
 		boolean init = params['init'] == "1"
 		String target =  params['target'] ?: null
 		boolean tree = params['tree'] == "1"
-		
+
 		if(init) {
-			log.debug("Init: Elfinder")			
+			log.debug("Init: Elfinder")
 			putResponse("api", "2.0")
 			putResponse("netDrivers", [])
-			
+
 			if(!target) {
 				target = hash(elFinderFileManager.root)
 			}
-		}					
-		
+		}
+
 		if(!target) {
 			addError("File not found")
-			return			
-		} 
-		
+			return
+		}
+
 		putResponse("uplMaxSize ", "1M")
-		 		
+
 		cwd(target)
 		files(target, tree)
-				
-		putResponse("options", elFinderFileManager.options(unhash(target)))				
+
+		putResponse("options", elFinderFileManager.options(unhash(target)))
 	}
 }
